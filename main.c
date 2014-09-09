af8160c5940db7bc1f96294507d2b5127aa9022a
Ethan Durrant <emd519@yahoo.com>
2014-09-09 15:52:09 -0700

    made a .gitignore for binary files

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include "USART.h"
 
 
+uint8_t flag_store = 0;
+#define FIRST_RUN_BIT 0 //bit of the first run flag
 
 /*An array of maximum track positions for each step-control pin.  Even pins
  are used for control, so only even numbers need a value here.  3.5" Floppies have
@@ -19,13 +21,23 @@ uint8_t currentPosition[] = {
   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
   
 //prototypes
-static inline void tick(void);
+inline void tick(void);
+void reset(void);
+void resetAll(void);
 
 void main(void)
 {
     //setup stuff
     
+    //setup step pins' port for output
+    STEP_DDR = 0xFF;
+    //setup direction pins' port for output
+    DIR_DDR = 0xFF;
     
+    
+    
+    //init the USART module and stuff
+    initUSART();
     
     //infinite loop
     while(1){
--- main.h
+++ main.h
@@ -7,3 +7,11 @@
 #include <avr/sleep.h>
 #include <avr/wdt.h>
 #include <avr/power.h>
+
+
+#define STEP_DDR DDRA
+#define STEP_PORT PORTA
+
+#define DIR_DDR DDRC
+#define DIR_PORT PORTC
+
